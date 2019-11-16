acf81d7cdbbc63dec335b075cac24a8c13e3333c
J.T <gisfromscratch@live.de>
2019-11-16 18:19:48 +0100

    CMD input and output, notr displayed in VS Code

--- src/rust_shell/src/main.rs
+++ src/rust_shell/src/main.rs
@@ -1,4 +1,14 @@
+use std::io;
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("Please guess the number.");
+
+    let mut number = String::new();
+
+    io::stdin().read_line(&mut number).expect("Failed to get the input!");
+
+    println!("You guessed: {}", number);
 }
 
