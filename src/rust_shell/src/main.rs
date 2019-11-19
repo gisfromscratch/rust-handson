a64b54d885862d4eed04e08e689627124fd934f3
J.T <gisfromscratch@live.de>
2019-11-19 18:16:55 +0100

    fixes #3

--- src/rust_shell/src/main.rs
+++ src/rust_shell/src/main.rs
@@ -4,23 +4,24 @@ use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
-    println!("Please guess the number.");
-
-    let mut number = String::new();
+    println!("Please guess the number.");    
 
     let secret_number = rand::thread_rng().gen_range(1, 10);
 
-    io::stdin().read_line(&mut number).expect("Failed to get the input!");
+    loop {
+        let mut number = String::new();
+        io::stdin().read_line(&mut number).expect("Failed to get the input!");
 
-    // Shadowing the variable named number
-    let number: u32 = number.trim().parse().expect("Input must be a number!");
+        // Shadowing the variable named number
+        let number: u32 = number.trim().parse().expect("Input must be a number!");
 
-    match number.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match number.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win!"),
+        }
+
+        println!("You guessed: {} and it was: {}", number, secret_number);
     }
-
-    println!("You guessed: {} and it was: {}", number, secret_number);
 }
 
